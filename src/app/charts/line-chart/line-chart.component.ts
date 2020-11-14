41df7991d7c7315f19333ef2cfeefe24f6fe44a1
Waleed Bin Khalid <62896922+waleedbinkhalid74@users.noreply.github.com>
2020-11-14 15:21:53 +0100

    Implemented bottom line chart

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [5, 4, 9, 7, 1], label: "Sales Volume"}
+];
+
+const LINE_CHART_SAMPLE_LABELS: string[] = ["2016", "2017", "2018", "2019", "2020"];
+
 
 @Component({
   selector: 'app-line-chart',
@@ -11,5 +18,13 @@ export class LineChartComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_SAMPLE_LABELS;
+  lineChartOptions: any = {
+    responsive : true,
+    maintainAspectRatio: false
+  };
+  lineChartLegend = true;
+  lineChartType = 'line'
+  lineChartColors = LINE_CHART_COLORS;
 }
---
+++ src/app/shared/chart.colors.ts
@@ -0,0 +1,10 @@
+export const LINE_CHART_COLORS = [
+    {
+      backgroundColor: 'rgba(6,214,160,0.2)',
+      pointBackgroundColor: '#000',
+      pointBordereColor: '#000',
+      pointHoverBackgroundColor: '#555',
+      pointHoverBorderColor: '#555'
+  
+    }
+  ];
