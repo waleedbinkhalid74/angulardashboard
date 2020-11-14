640734bd849a5af4af98e1761c840205d8c1602b
Waleed Bin Khalid <62896922+waleedbinkhalid74@users.noreply.github.com>
2020-11-14 15:55:23 +0100

    Implemented tables

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Order } from '../../shared/order';
 @Component({
   selector: 'app-section-orders',
   templateUrl: './section-orders.component.html',
@@ -8,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
-
+  orders: Order[] = [
+    {id: 1, customer: {id: 1, name:"Waleed" ,state: "New York", email: 'waleed.binkhalid@hotmail.com'}, 
+    total: 230, placed:new Date(2020, 11,1), fulfilled: new Date(2020, 12,1)},
+    {id: 2, customer: {id: 1, name:"Waleed" ,state: "New York", email: 'waleed.binkhalid@hotmail.com'}, 
+    total: 230, placed:new Date(2020, 12,1), fulfilled: new Date(2020, 12,1)},
+    {id: 3, customer: {id: 1, name:"Waleed" ,state: "New York", email: 'waleed.binkhalid@hotmail.com'}, 
+    total: 230, placed:new Date(2020, 12,1), fulfilled: new Date(2020, 12,1)},
+    {id: 4, customer: {id: 1, name:"Waleed" ,state: "New York", email: 'waleed.binkhalid@hotmail.com'}, 
+    total: 230, placed:new Date(2020, 12,1), fulfilled: new Date(2020, 12,1)},
+    {id: 5, customer: {id: 1, name:"Waleed" ,state: "New York", email: 'waleed.binkhalid@hotmail.com'}, 
+    total: 230, placed:new Date(2020, 12,1), fulfilled: new Date(2020, 12,1)},
+  ];
   ngOnInit(): void {
   }
 
