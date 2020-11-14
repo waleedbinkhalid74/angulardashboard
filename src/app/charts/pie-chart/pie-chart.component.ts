05e0106aef270d0165362f3663179e66b794d33a
Waleed Bin Khalid <62896922+waleedbinkhalid74@users.noreply.github.com>
2020-11-14 14:46:28 +0100

    Added Pie Charts

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,17 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number [] = [350, 450, 120];
+  pieChartLabels: string [] = ["Maier", "Huber", "Mueller"];
+  colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166' ],
+      borderColor: '#111'
+    }
+  ];
+
+  pieChartType = "pie";
+
   ngOnInit(): void {
   }
 
