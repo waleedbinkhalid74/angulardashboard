53ffa5e6d8531acf35cbe5358dbd8c3b9fde9711
Waleed Bin Khalid <62896922+waleedbinkhalid74@users.noreply.github.com>
2020-11-22 13:27:10 +0100

    Checkbox working for inner two levels. Top level left

--- src/app/article-hierarchy/article-hierarchy.component.ts
+++ src/app/article-hierarchy/article-hierarchy.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { ThemePalette } from '@angular/material/core';
 import { UserService } from '../user.service';
+import { trimTrailingNulls } from '@angular/compiler/src/render3/view/util';
 
 export interface Task {
   name: string;
@@ -51,25 +52,54 @@ export class ArticleHierarchyComponent implements OnInit {
     ]
   };
 
-  allComplete: boolean = false;
+  allComplete: boolean = true;
 
-  updateAllComplete() {
-    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
+  updateAllComplete(key1, key2, iterator) {
+    //    console.log(this.structuresArticleData[0][key1][key2].Article[iterator])
+    this.allComplete = this.structuresArticleData[0][key1][key2].Article != null && this.structuresArticleData[0][key1][key2].Article.every(t => t.completed);
+    console.log(this.structuresArticleData);
   }
 
-  someComplete(): boolean {
-    if (this.task.subtasks == null) {
-      return false;
+  someComplete(completed: boolean, key1, key2) {
+    //    console.log(this.structuresArticleData[0][key1][key2].Article);
+    //    console.log(this.structuresArticleData[0][key1][key2].IndeterStatus);
+    //    console.log(this.structuresArticleData[0][key1]);
+    //    return false;
+    console.log("HI");
+    if (this.structuresArticleData[0][key1][key2].Article == null) {
+      this.structuresArticleData[0][key1][key2].IndeterStatus = false;
+      //      this.structuresArticleData[0][key1].IndeterStatus = false;
     }
-    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
+    if (this.structuresArticleData[0][key1][key2].Article.filter(t => t.completed).length == this.structuresArticleData[0][key1][key2].Article.length) {
+      this.structuresArticleData[0][key1][key2].IndeterStatus = false;
+      this.structuresArticleData[0][key1].IndeterStatus = false;
+
+    }
+    else {
+      this.structuresArticleData[0][key1][key2].IndeterStatus = this.structuresArticleData[0][key1][key2].Article.filter(t => t.completed).length > 0;
+      this.structuresArticleData[0][key1].IndeterStatus = this.structuresArticleData[0][key1][key2].Article.filter(t => t.completed).length > 0;
+    }
+
   }
 
-  setAll(completed: boolean) {
+  setAll(completed: boolean, key1, key2) {
     this.allComplete = completed;
-    if (this.task.subtasks == null) {
+    if (this.structuresArticleData[0][key1][key2].Article == null) {
       return;
     }
-    this.task.subtasks.forEach(t => t.completed = completed);
+    this.structuresArticleData[0][key1][key2].Article.forEach(t => t.completed = completed);
+  }
+
+  setAll1(completed: boolean, key1) {
+    this.structuresArticleData[0][key1].GrISummary.forEach(element => {
+      element.completed=true;
+    }); ;
+    
+    // this.allComplete = completed;
+    // if (this.structuresArticleData[0][key1] == null) {
+    //   return;
+    // }
+    // this.structuresArticleData[0][key1].forEach(t => t.completed = completed);
   }
 
   constructor(private userService: UserService) { }
@@ -89,7 +119,7 @@ export class ArticleHierarchyComponent implements OnInit {
 
 function uniqueJSONGrII(jsonfile) {
   var lookupGrII = {};
-  var ans= [{}];
+  var ans = [{}];
   var result = [];
   for (var _j = 0; _j < jsonfile.length; _j++) {
     if (!(jsonfile[_j].GrII in lookupGrII)) {
@@ -104,18 +134,24 @@ function uniqueJSONGrII(jsonfile) {
 
 function uniqueJSONGrI(jsonfile, filterElement) {
   var lookupGrI = {};
-  var ans= {};
+  var ans = {};
   var result = [];
+  var result2 = [];
   for (var _j = 0; _j < jsonfile.length; _j++) {
-    if (jsonfile[_j].GrII==filterElement && !(jsonfile[_j].GrI in lookupGrI)) {
+    if (jsonfile[_j].GrII == filterElement && !(jsonfile[_j].GrI in lookupGrI)) {
       ans[jsonfile[_j].GrI] = uniqueJSONArtBz(jsonfile, jsonfile[_j].GrI, jsonfile[_j].GrII);
       var temp = jsonfile[_j].GrI;
       lookupGrI[jsonfile[_j].GrI] = 1;
-      result.push(jsonfile[_j].GrI
-        );
+      result.push(jsonfile[_j].GrI);
+      result2.push({
+        name: jsonfile[_j].GrI,
+        completed: true
+      });
     }
     ans["GrI"] = result;
-
+    ans["IndeterStatus"] = false;
+    ans["completed"] = true;
+    ans["GrISummary"] = result2;
   }
   return ans;
 }
@@ -123,19 +159,40 @@ function uniqueJSONGrI(jsonfile, filterElement) {
 function uniqueJSONArtBz(jsonfile, filterElement1, filterElement2) {
   var lookupArtBz = {};
   var result = [];
-  var ans= {};
+  var result2 = [];
+  var ans = {};
   //jsonfile[_j].GrII==filterElement1 && jsonfile[_j].GrI==filterElement2 && 
   for (var _j = 0; _j < jsonfile.length; _j++) {
-    if (jsonfile[_j].GrI==filterElement1 && jsonfile[_j].GrII==filterElement2) {
-      ans[jsonfile[_j].ArtBez] = "completed";
+    if (jsonfile[_j].GrI == filterElement1 && jsonfile[_j].GrII == filterElement2) {
+      //      ans[jsonfile[_j].ArtBez] = "completed";
+      result2 = uniqueJSONAssignComplete(jsonfile, jsonfile[_j].GrI, jsonfile[_j].GrII, jsonfile[_j].ArtBez);
       lookupArtBz[jsonfile[_j].ArtBz] = 1;
       result.push(
         jsonfile[_j].ArtBez
-        );
+      );
     }
     ans["ArtBez"] = result;
+    ans["Article"] = result2;
+    ans["IndeterStatus"] = false;
+    ans["completed"] = true;
   }
   return ans;
+}
+
+function uniqueJSONAssignComplete(jsonfile, filterElement1, filterElement2, filterElement3) {
+  var lookupArtBz = {};
+  var result = [];
+  var ans = {};
+  for (var _j = 0; _j < jsonfile.length; _j++) {
+    if (jsonfile[_j].GrI == filterElement1 && jsonfile[_j].GrII == filterElement2) {
+      result.push({
+        name: jsonfile[_j].ArtBez,
+        completed: true
+      });
+    }
+  }
+  //  console.log(result)
+  return result;
 }
 /* task: Task = {
     name: 'Indeterminate',
---
+++ src/app/article-hierarchy/tempCodeRunnerFile.ts
@@ -0,0 +1,4 @@
+    if (this.task.subtasks == null) {
+      return false;
+    }
+    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
